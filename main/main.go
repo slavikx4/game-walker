7e7cc0a5d0f643f80be4a05c7ca8c641718e4375
slava <slava@DESKTOP-44TVUCI>
2023-04-16 15:54:24 +0300

    убрал из main глобальные переменные, вызвал бота в main

--- main/main.go
+++ main/main.go
@@ -2,47 +2,15 @@ package main
 
 import (
 	"game-walker/src"
-	"strings"
+	"game-walker/telegram"
+	"log"
 )
-
-var players []*src.Player
-var rooms src.Room
-
-func initGame() {
-	rooms = src.NewRoom()
-}
-
-func addPlayer(newPlayer *src.Player) {
-	rooms.Mu.Lock()
-	players = append(players, newPlayer)
-	rooms.Mu.Unlock()
-	go func(p *src.Player) {
-		var command string
-		for {
-			select {
-			case command = <-p.ChannelInput:
-				answer := handleCommand(command, newPlayer)
-				if answer != "" {
-					p.HandleOutput(answer)
-				}
-			}
-		}
-	}(newPlayer)
-	rooms.Mu.Lock()
-	rooms.KitchenRoom.InRoom = append(rooms.KitchenRoom.InRoom, newPlayer)
-	rooms.Mu.Unlock()
-}
-
-func handleCommand(command string, player *src.Player) string {
-	commands := strings.Fields(command)
-	f, ok := player.Do[commands[0]]
-	if !ok {
-		return "неизвестная команда"
-	}
-	answer := f(commands, player, &rooms)
-	return answer
-}
 
 func main() {
 
+	src.StartWorld()
+	log.Println("initGame: OK")
+
+	log.Println("Start work bot")
+	telegram.StartBot()
 }
